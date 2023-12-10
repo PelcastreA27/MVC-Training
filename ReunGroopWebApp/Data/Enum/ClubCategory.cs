f942940b41cdb9a1079e4f67e6352438023419a0
PelcastreA27 <pelcortes@gmail.com>
2023-12-10 00:42:39 -0600

    Agregar archivos de proyecto.

---
+++ ReunGroopWebApp/Data/Enum/ClubCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace ReunGroopWebApp.Data.Enum
+{
+    //enum coloca como enteros las categorias ejemp RoadRunner 1, 2,3,4 etc.
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ ReunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ReunGroopWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        //Para las variables con signo de ? en el tipo de dato int?, bool? refiere a que puede tener valores nullos.
+        public int? Pace { get; set; }
+
+        public int? Mileage { get; set; }
+        public Address Address { get; set; }
+
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+
+    }
+}
